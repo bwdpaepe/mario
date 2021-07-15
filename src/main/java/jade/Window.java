21f6fac89b32b5536dcaea2f85dbd0535114ca22
BART DE PAEPE <BART.DEPAEPE@schweb.be>
2021-07-15 08:24:03 +0200

    scene manager

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,6 +3,7 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -16,8 +17,10 @@ public class Window {
     private long glfwWindow;
     private static Window window = null;
 
-    private float r, g, b, a;
+    public float r, g, b, a;
     boolean fadeToBlack = false;
+
+    private static Scene currentScene;
 
     private Window() {
         this.width = 1920;
@@ -27,6 +30,20 @@ public class Window {
         this.g = 1;
         this.b = 1;
         this.a = 1;
+    }
+
+    public static void changeScene(int newScene) {
+        switch(newScene){
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false : "Unknown scene '" + newScene + "'";
+        }
     }
 
     public static Window get() {
@@ -91,10 +108,13 @@ public class Window {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
-
+        Window.changeScene(0);
     }
 
     public void loop() {
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+        float dt = -1.0f;
         while (!glfwWindowShouldClose(glfwWindow)) {
             // Poll events
             glfwPollEvents();
@@ -102,7 +122,7 @@ public class Window {
             glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if (fadeToBlack) {
+            /*if (fadeToBlack) {
                 r = Math.max(r - 0.01f,0);
                 g = Math.max(g - 0.01f,0);
                 b = Math.max(b - 0.01f,0);
@@ -111,9 +131,17 @@ public class Window {
             if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
                 //System.out.println("Space key is pressed");
                 fadeToBlack = true;
+            }*/
+
+            if(dt>=0){
+                currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
 
         }
     }
