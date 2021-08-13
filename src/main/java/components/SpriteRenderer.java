370653b09fc7703d6fb6b2b1749d0c41db6a2b34
BART DE PAEPE <BART.DEPAEPE@schweb.be>
2021-08-13 23:15:21 +0200

    ImGui fonts and scene

--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -1,5 +1,6 @@
 package components;
 
+import imgui.ImGui;
 import jade.Component;
 import jade.Transform;
 import org.joml.Vector2f;
@@ -41,6 +42,15 @@ public class SpriteRenderer extends Component {
             System.out.println("I am updating");
             this.firstTime = true;
         }*/
+    }
+
+    @Override
+    public void imgui() {
+        float[] imColor = {color.x, color.y, color.z, color.w};
+        if(ImGui.colorPicker4("Color Picker", imColor)) {
+            this.color.set(imColor[0], imColor[1], imColor[2], imColor[3]);
+            this.isDirty = true;
+        }
     }
 
     public Vector4f getColor() {
